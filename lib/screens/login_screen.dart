1e509ddcb8b48b21389350cc6121d94181844a96
zhyar <zhear111@gmail.com>
2023-05-14 14:54:12 +0300

    Initial commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const RegisterScreen(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
